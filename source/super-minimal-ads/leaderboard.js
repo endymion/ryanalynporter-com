ab2a933e08b4bafd0b90d4d55a775f85ebee9089
Ryan Alyn Porter <rap@endymion.com>
2013-01-27 15:43:11 -0500

    Fixed .gif -> .png

--- source/super-minimal-ads/leaderboard.js
+++ source/super-minimal-ads/leaderboard.js
@@ -1,26 +1,26 @@
 var banners = [
   {
-    "img": "tnl-banner-steve-jobs-01.gif",
+    "img": "tnl-banner-steve-jobs-01.png",
     "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
     "weight": 4
   },
   {
-    "img": "tnl-banner-steve-jobs-02.gif",
+    "img": "tnl-banner-steve-jobs-02.png",
     "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
     "weight": 1
   },
   {
-    "img": "tnl-banner-steve-jobs-03.gif",
+    "img": "tnl-banner-steve-jobs-03.png",
     "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
     "weight": 1
   },
   {
-    "img": "tnl-banner-steve-jobs-04.gif",
+    "img": "tnl-banner-steve-jobs-04.png",
     "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
     "weight": 1
   },
   {
-    "img": "tnl-banner-steve-jobs-05.gif",
+    "img": "tnl-banner-steve-jobs-05.png",
     "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
     "weight": 1
   }
